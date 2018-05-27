21476a180211435ebb5fe3a5dc07ab113f998111
HanneLEE <38913004+HanneLEE@users.noreply.github.com>
2018-05-27 20:23:18 +0800

    Update and rename .gitignore to .m

---
+++ .gitignore/.m
@@ -0,0 +1,385 @@
+clear;
+data_set=[5.1,3.5,1.4,0.2,1;4.9,3.0,1.4,0.2,1;4.7,3.2,1.3,0.2,1;4.6,3.1,1.5,0.2,1;5.0,3.6,1.4,0.2,1;5.4,3.9,1.7,0.4,1;4.6,3.4,1.4,0.3,1;5.0,3.4,1.5,0.2,1;4.4,2.9,1.4,0.2,1;4.9,3.1,1.5,0.1,1;5.4,3.7,1.5,0.2,1;4.8,3.4,1.6,0.2,1;4.8,3.0,1.4,0.1,1;4.3,3.0,1.1,0.1,1;5.8,4.0,1.2,0.2,1;5.7,4.4,1.5,0.4,1;5.4,3.9,1.3,0.4,1;5.1,3.5,1.4,0.3,1;5.7,3.8,1.7,0.3,1;5.1,3.8,1.5,0.3,1;5.4,3.4,1.7,0.2,1;5.1,3.7,1.5,0.4,1;4.6,3.6,1.0,0.2,1;5.1,3.3,1.7,0.5,1;4.8,3.4,1.9,0.2,1;5.0,3.0,1.6,0.2,1;5.0,3.4,1.6,0.4,1;5.2,3.5,1.5,0.2,1;5.5,4.2,1.4,0.2,1;4.9,3.1,1.5,0.1,1;5.0,3.2,1.2,0.2,1;5.5,3.5,1.3,0.2,1;4.9,3.1,1.5,0.1,1;4.4,3.0,1.3,0.2,1;5.1,3.4,1.5,0.2,1;5.0,3.5,1.3,0.3,1;4.5,2.3,1.3,0.3,1;4.4,3.2,1.3,0.2,1;5.0,3.5,1.6,0.6,1;5.1,3.8,1.9,0.4,1;4.8,3.0,1.4,0.3,1;5.1,3.8,1.6,0.2,1;4.6,3.2,1.4,0.2,1;5.3,3.7,1.5,0.2,1;5.0,3.3,1.4,0.2,1;7.0,3.2,4.7,1.4,2;6.4,3.2,4.5,1.5,2;6.9,3.1,4.9,1.5,2;5.5,2.3,4.0,1.3,2;6.5,2.8,4.6,1.5,2;5.7,2.8,4.5,1.3,2;6.3,3.3,4.7,1.6,2;4.9,2.4,3.3,1.0,2;6.6,2.9,4.6,1.3,2;5.2,2.7,3.9,1.4,2;5.0,2.0,3.5,1.0,2;5.9,3.0,4.2,1.5,2;6.0,2.2,4.0,1.0,2;6.1,2.9,4.7,1.4,2;5.6,2.9,3.6,1.3,2;6.7,3.1,4.4,1.4,2;5.6,3.0,4.5,1.5,2;5.8,2.7,4.1,1.0,2;6.2,2.2,4.5,1.5,2;5.6,2.5,3.9,1.1,2;5.9,3.2,4.8,1.8,2;6.1,2.8,4.0,1.3,2;6.3,2.5,4.9,1.5,2;6.1,2.8,4.7,1.2,2;6.4,2.9,4.3,1.3,2;6.6,3.0,4.4,1.4,2;6.8,2.8,4.8,1.4,2;6.7,3.0,5.0,1.7,2;6.0,2.9,4.5,1.5,2;5.7,2.6,3.5,1.0,2;6.3,2.3,4.4,1.3,2;5.6,3.0,4.1,1.3,2;5.5,2.5,4.0,1.3,2;5.5,2.6,4.4,1.2,2;6.1,3.0,4.6,1.4,2;5.8,2.6,4.0,1.2,2;5.0,2.3,3.3,1.0,2;5.6,2.7,4.2,1.3,2;5.7,3.0,4.2,1.2,2;5.7,2.9,4.2,1.3,2;6.2,2.9,4.3,1.3,2;5.1,2.5,3.0,1.1,2;5.7,2.8,4.1,1.3,2;6.3,3.3,6.0,2.5,3;5.8,2.7,5.1,1.9,3;7.1,3.0,5.9,2.1,3;6.3,2.9,5.6,1.8,3;6.5,3.0,5.8,2.2,3;7.6,3.0,6.6,2.1,3;4.9,2.5,4.5,1.7,3;7.3,2.9,6.3,1.8,3;6.7,2.5,5.8,1.8,3;7.2,3.6,6.1,2.5,3;6.5,3.2,5.1,2.0,3;6.4,2.7,5.3,1.9,3;6.8,3.0,5.5,2.1,3;5.7,2.5,5.0,2.0,3;5.8,2.8,5.1,2.4,3;6.4,3.2,5.3,2.3,3;6.5,3.0,5.5,1.8,3;7.7,3.8,6.7,2.2,3;7.7,2.6,6.9,2.3,3;6.0,2.2,5.0,1.5,3;6.9,3.2,5.7,2.3,3;5.6,2.8,4.9,2.0,3;7.7,2.8,6.7,2.0,3;6.3,2.7,4.9,1.8,3;6.7,3.3,5.7,2.1,3;7.2,3.2,6.0,1.8,3;6.2,2.8,4.8,1.8,3;6.1,3.0,4.9,1.8,3;6.4,2.8,5.6,2.1,3;7.2,3.0,5.8,1.6,3;7.4,2.8,6.1,1.9,3;7.9,3.8,6.4,2.0,3;6.4,2.8,5.6,2.2,3;6.3,2.8,5.1,1.5,3;6.0,3.0,4.8,1.8,3;6.9,3.1,5.4,2.1,3;6.7,3.1,5.6,2.4,3;6.9,3.1,5.1,2.3,3;5.8,2.7,5.1,1.9,3;6.8,3.2,5.9,2.3,3;6.7,3.3,5.7,2.5,3;6.7,3.0,5.2,2.3,3;6.3,2.5,5.0,1.9,3;6.5,3.0,5.2,2.0,3;6.2,3.4,5.4,2.3,3;5.9,3.0,5.1,1.8,3];
+
+data_sl=data_set(:,1);
+data_sw=data_set(:,2);
+data_pl=data_set(:,3);
+data_pw=data_set(:,4);
+data_class=data_set(:,5);
+% Fsl=tabulate(data_sl(:));%统计频数与频率
+% Fsw=tabulate(data_sw(:));
+% Fpl=tabulate(data_pl(:));
+% Fpw=tabulate(data_pw(:));
+% p_sl_c=zeros(size(Fsl(:,1),1),3);
+% p_sw_c=zeros(size(Fsw(:,1),1),3);
+% p_pl_c=zeros(size(Fpl(:,1),1),3);
+% p_pw_c=zeros(size(Fpw(:,1),1),3);
+% for  j=1:size(Fsl(:,1),1)%计算花萼长频数
+%   for i=1:size(data_sl,1)
+%     if (data_sl(i)==Fsl(j,1)&&data_class(i)==1)
+%         p_sl_c(j,1)=p_sl_c(j,1)+1;
+%     elseif(data_sl(i)==Fsl(j,1)&&data_class(i)==2)
+%         p_sl_c(j,2)=p_sl_c(j,2)+1;
+%     elseif(data_sl(i)==Fsl(j,1)&&data_class(i)==3)
+%         p_sl_c(j,3)=p_sl_c(j,3)+1;
+%     end
+%   end
+% end
+% 
+%   for  j=1:size(Fsw(:,1),1)%计算花萼宽频数
+%   for i=1:size(data_sw,1)
+%     if (data_sw(i)==Fsw(j,1)&&data_class(i)==1)
+%         p_sw_c(j,1)=p_sw_c(j,1)+1;
+%     elseif(data_sw(i)==Fsw(j,1)&&data_class(i)==2)
+%         p_sw_c(j,2)=p_sw_c(j,2)+1;
+%     elseif(data_sw(i)==Fsw(j,1)&&data_class(i)==3)
+%         p_sw_c(j,3)=p_sw_c(j,3)+1;
+%     end
+%   end
+%   end
+%   
+% for  j=1:size(Fpl(:,1),1)%计算花瓣长频数
+%   for i=1:size(data_pl,1)
+%     if (data_pl(i)==Fpl(j,1)&&data_class(i)==1)
+%         p_pl_c(j,1)=p_pl_c(j,1)+1;
+%     elseif(data_pl(i)==Fpl(j,1)&&data_class(i)==2)
+%         p_pl_c(j,2)=p_pl_c(j,2)+1;
+%     elseif(data_pl(i)==Fpl(j,1)&&data_class(i)==3)
+%         p_pl_c(j,3)=p_pl_c(j,3)+1;
+%     end
+%   end
+% end
+% 
+% for  j=1:size(Fpw(:,1),1)%计算花瓣宽频数
+%   for i=1:size(data_pw,1)
+%     if (data_pw(i)==Fpw(j,1)&&data_class(i)==1)
+%         p_pw_c(j,1)=p_pw_c(j,1)+1;
+%     elseif(data_pw(i)==Fpw(j,1)&&data_class(i)==2)
+%         p_pw_c(j,2)=p_pw_c(j,2)+1;
+%     elseif(data_pw(i)==Fpw(j,1)&&data_class(i)==3)
+%         p_pw_c(j,3)=p_pw_c(j,3)+1;
+%     end
+%   end
+% end
+% 
+% Fslsum=[Fsl p_sl_c];%统计频数与频率
+% Fswsum=[Fsw p_sw_c];
+% Fplsum=[Fpl p_pl_c];
+% Fpwsum=[Fpw p_pw_c];
+
+% %计算总信息熵
+% Esl=-(sum(Fslsum(:,4))/sum(Fslsum(:,2)))*log2((sum(Fslsum(:,4))/sum(Fslsum(:,2))))-(sum(Fslsum(:,5))/sum(Fslsum(:,2)))*log2((sum(Fslsum(:,5))/sum(Fslsum(:,2))))-(sum(Fslsum(:,6))/sum(Fslsum(:,2)))*log2((sum(Fslsum(:,6))/sum(Fslsum(:,2))));
+% Esw=-(sum(Fswsum(:,4))/sum(Fswsum(:,2)))*log2((sum(Fswsum(:,4))/sum(Fswsum(:,2))))-(sum(Fswsum(:,5))/sum(Fswsum(:,2)))*log2((sum(Fswsum(:,5))/sum(Fswsum(:,2))))-(sum(Fswsum(:,6))/sum(Fswsum(:,2)))*log2((sum(Fswsum(:,6))/sum(Fswsum(:,2))));
+% Epl=-(sum(Fplsum(:,4))/sum(Fplsum(:,2)))*log2((sum(Fplsum(:,4))/sum(Fplsum(:,2))))-(sum(Fplsum(:,5))/sum(Fplsum(:,2)))*log2((sum(Fplsum(:,5))/sum(Fplsum(:,2))))-(sum(Fplsum(:,6))/sum(Fplsum(:,2)))*log2((sum(Fplsum(:,6))/sum(Fplsum(:,2))));
+% Epw=-(sum(Fpwsum(:,4))/sum(Fpwsum(:,2)))*log2((sum(Fpwsum(:,4))/sum(Fpwsum(:,2))))-(sum(Fpwsum(:,5))/sum(Fpwsum(:,2)))*log2((sum(Fpwsum(:,5))/sum(Fpwsum(:,2))))-(sum(Fpwsum(:,6))/sum(Fpwsum(:,2)))*log2((sum(Fpwsum(:,6))/sum(Fpwsum(:,2))));
+% 
+% 
+% Esl_fen=0;
+% Esw_fen=0;
+% Epl_fen=0;
+% Epw_fen=0;
+% %计算sl分信息熵的和
+% for i=1:size(Fslsum,1)
+%     for j=4:6
+%         if(Fslsum(i,j)~=0)
+%         Esl_fen=Esl_fen+Fslsum(i,2)/sum(Fslsum(:,2))*(-Fslsum(i,j)/Fslsum(i,2)*log2(Fslsum(i,j)/Fslsum(i,2)));
+%         else
+%             Esl_fen=Esl_fen+0;
+%         end
+%     end
+% end
+%  
+% 
+% %计算sw分信息熵的和
+% for i=1:size(Fswsum,1)
+%     for j=4:6
+%         if(Fswsum(i,j)~=0)
+%         Esw_fen=Esw_fen+Fswsum(i,2)/sum(Fswsum(:,2))*(-Fswsum(i,j)/Fswsum(i,2)*log2(Fswsum(i,j)/Fswsum(i,2)));
+%         else
+%             Esw_fen=Esw_fen+0;
+%         end
+%     end
+% end
+% 
+% %计算pl分信息熵的和
+% for i=1:size(Fplsum,1)
+%     for j=4:6
+%         if(Fplsum(i,j)~=0)
+%         Epl_fen=Epl_fen+Fplsum(i,2)/sum(Fplsum(:,2))*(-Fplsum(i,j)/Fplsum(i,2)*log2(Fplsum(i,j)/Fplsum(i,2)));
+%         else
+%             Epl_fen=Epl_fen+0;
+%         end
+%     end
+% end
+% 
+% %计算pw分信息熵的和
+% for i=1:size(Fpwsum,1)
+%     for j=4:6
+%         if(Fpwsum(i,j)~=0)
+%         Epw_fen=Epw_fen+Fpwsum(i,2)/sum(Fpwsum(:,2))*(-Fpwsum(i,j)/Fpwsum(i,2)*log2(Fpwsum(i,j)/Fpwsum(i,2)));
+%         else
+%             Epw_fen=Epw_fen+0;
+%         end
+%     end
+% end
+% %计算信息增益
+% Gsl=Esl-Esl_fen;
+% Gsw=Esw-Esw_fen;
+% Gpl=Epl-Epl_fen;
+% Gpw=Epw-Epw_fen;
+
+
+
+%剔除已分类数据，重组数据，二轮计算
+ k=1;
+for i=1:size(data_sl)
+    if(data_sl(i)>4.8&&data_sl(i)<7.1)
+    data_sw_2(k)=data_sw(i);
+    k=k+1;
+    end
+end
+
+ k=1;
+for i=1:size(data_sl)
+    if(data_sl(i)>4.8&&data_sl(i)<7.1)
+    data_pl_2(k)=data_pl(i);
+    k=k+1;
+    end
+end
+
+ k=1;
+for i=1:size(data_sl)
+    if(data_sl(i)>4.8&&data_sl(i)<7.1)
+    data_pw_2(k)=data_pw(i);
+    k=k+1;
+    end
+end
+
+
+ k=1;
+for i=1:size(data_sl)
+    if(data_sl(i)>4.8&&data_sl(i)<7.1)
+    data_class_2(k)=data_class(i);
+    k=k+1;
+    end
+end
+
+
+data_sw_2=data_sw_2';
+data_pl_2=data_pl_2';
+data_pw_2=data_pw_2';
+data_class_2=data_class_2';
+
+
+% Fsw_2=tabulate(data_sw_2(:));
+% Fpl_2=tabulate(data_pl_2(:));
+% Fpw_2=tabulate(data_pw_2(:));
+% p_sw_c_2=zeros(size(Fsw_2(:,1),1),3);
+% p_pl_c_2=zeros(size(Fpl_2(:,1),1),3);
+% p_pw_c_2=zeros(size(Fpw_2(:,1),1),3);
+% 
+%  for  j=1:size(Fsw_2(:,1),1)%计算二轮花萼宽频数
+%   for i=1:size(data_sw_2,1)
+%     if (data_sw_2(i)==Fsw_2(j,1)&&data_class_2(i)==1)
+%         p_sw_c_2(j,1)=p_sw_c_2(j,1)+1;
+%     elseif(data_sw_2(i)==Fsw_2(j,1)&&data_class_2(i)==2)
+%         p_sw_c_2(j,2)=p_sw_c_2(j,2)+1;
+%     elseif(data_sw_2(i)==Fsw_2(j,1)&&data_class_2(i)==3)
+%         p_sw_c_2(j,3)=p_sw_c_2(j,3)+1;
+%     end
+%   end
+%  end
+%  
+%  
+%  for  j=1:size(Fpl_2(:,1),1)%计算二轮花瓣长频数
+%   for i=1:size(data_pl_2,1)
+%     if (data_pl_2(i)==Fpl_2(j,1)&&data_class_2(i)==1)
+%         p_pl_c_2(j,1)=p_pl_c_2(j,1)+1;
+%     elseif(data_pl_2(i)==Fpl_2(j,1)&&data_class_2(i)==2)
+%         p_pl_c_2(j,2)=p_pl_c_2(j,2)+1;
+%     elseif(data_pl_2(i)==Fpl_2(j,1)&&data_class_2(i)==3)
+%         p_pl_c_2(j,3)=p_pl_c_2(j,3)+1;
+%     end
+%   end
+% end
+% 
+% for  j=1:size(Fpw_2(:,1),1)%计算二轮花瓣宽频数
+%   for i=1:size(data_pw_2,1)
+%     if (data_pw_2(i)==Fpw_2(j,1)&&data_class_2(i)==1)
+%         p_pw_c_2(j,1)=p_pw_c_2(j,1)+1;
+%     elseif(data_pw_2(i)==Fpw_2(j,1)&&data_class_2(i)==2)
+%         p_pw_c_2(j,2)=p_pw_c_2(j,2)+1;
+%     elseif(data_pw_2(i)==Fpw_2(j,1)&&data_class_2(i)==3)
+%         p_pw_c_2(j,3)=p_pw_c_2(j,3)+1;
+%     end
+%   end
+% end
+% 
+% %统计二轮频数与频率
+% Fswsum_2=[Fsw_2 p_sw_c_2];
+% Fplsum_2=[Fpl_2 p_pl_c_2];
+% Fpwsum_2=[Fpw_2 p_pw_c_2];
+% 
+% %计算总信息熵
+% Esw_2=-(sum(Fswsum_2(:,4))/sum(Fswsum_2(:,2)))*log2((sum(Fswsum_2(:,4))/sum(Fswsum_2(:,2))))-(sum(Fswsum_2(:,5))/sum(Fswsum_2(:,2)))*log2((sum(Fswsum_2(:,5))/sum(Fswsum_2(:,2))))-(sum(Fswsum_2(:,6))/sum(Fswsum_2(:,2)))*log2((sum(Fswsum_2(:,6))/sum(Fswsum_2(:,2))));
+% Epl_2=-(sum(Fplsum_2(:,4))/sum(Fplsum_2(:,2)))*log2((sum(Fplsum_2(:,4))/sum(Fplsum_2(:,2))))-(sum(Fplsum_2(:,5))/sum(Fplsum_2(:,2)))*log2((sum(Fplsum_2(:,5))/sum(Fplsum_2(:,2))))-(sum(Fplsum_2(:,6))/sum(Fplsum_2(:,2)))*log2((sum(Fplsum_2(:,6))/sum(Fplsum_2(:,2))));
+% Epw_2=-(sum(Fpwsum_2(:,4))/sum(Fpwsum_2(:,2)))*log2((sum(Fpwsum_2(:,4))/sum(Fpwsum_2(:,2))))-(sum(Fpwsum_2(:,5))/sum(Fpwsum_2(:,2)))*log2((sum(Fpwsum_2(:,5))/sum(Fpwsum_2(:,2))))-(sum(Fpwsum_2(:,6))/sum(Fpwsum_2(:,2)))*log2((sum(Fpwsum_2(:,6))/sum(Fpwsum_2(:,2))));
+% 
+% 
+% 
+% Esl_fen_2=0;
+% Esw_fen_2=0;
+% Epl_fen_2=0;
+% Epw_fen_2=0;
+% 
+% %计算sw_2分信息熵的和
+% for i=1:size(Fswsum_2,1)
+%     for j=4:6
+%         if(Fswsum_2(i,j)~=0)
+%         Esw_fen_2=Esw_fen_2+Fswsum_2(i,2)/sum(Fswsum_2(:,2))*(-Fswsum_2(i,j)/Fswsum_2(i,2)*log2(Fswsum_2(i,j)/Fswsum_2(i,2)));
+%         else
+%             Esw_fen_2=Esw_fen_2+0;
+%         end
+%     end
+% end
+% 
+% 
+% %计算pl_2分信息熵的和
+% for i=1:size(Fplsum_2,1)
+%     for j=4:6
+%         if(Fplsum_2(i,j)~=0)
+%         Epl_fen_2=Epl_fen_2+Fplsum_2(i,2)/sum(Fplsum_2(:,2))*(-Fplsum_2(i,j)/Fplsum_2(i,2)*log2(Fplsum_2(i,j)/Fplsum_2(i,2)));
+%         else
+%             Epl_fen_2=Epl_fen_2+0;
+%         end
+%     end
+% end
+% 
+% %计算pw_2分信息熵的和
+% for i=1:size(Fpwsum_2,1)
+%     for j=4:6
+%         if(Fpwsum_2(i,j)~=0)
+%         Epw_fen_2=Epw_fen_2+Fpwsum_2(i,2)/sum(Fpwsum_2(:,2))*(-Fpwsum_2(i,j)/Fpwsum_2(i,2)*log2(Fpwsum_2(i,j)/Fpwsum_2(i,2)));
+%         else
+%             Epw_fen_2=Epw_fen_2+0;
+%         end
+%     end
+% end
+% %计算信息增益
+% Gsw_2=Esw_2-Esw_fen_2;
+% Gpl_2=Epl_2-Epl_fen_2;
+% Gpw_2=Epw_2-Epw_fen_2;
+
+
+
+
+
+
+%剔除已分类数据，重组数据，三轮计算
+ k=1;
+for i=1:size(data_pw_2)
+    if(data_pw(i)>1.6&&data_pw(i)<1.9)
+    data_sw_3(k)=data_sw_2(i);
+    k=k+1;
+    end
+end
+
+ k=1;
+for i=1:size(data_pw_2)
+    if(data_pw(i)>1.6&&data_pw(i)<1.9)
+    data_pl_3(k)=data_pl_2(i);
+    k=k+1;
+    end
+end
+
+
+
+ k=1;
+for i=1:size(data_pw_2)
+    if(data_pw(i)>1.6&&data_pw(i)<1.9)
+    data_class_3(k)=data_class_2(i);
+    k=k+1;
+    end
+end
+
+
+data_sw_3=data_sw_3';
+data_pl_3=data_pl_3';
+data_class_3=data_class_3';
+
+
+Fsw_3=tabulate(data_sw_3(:));
+Fpl_3=tabulate(data_pl_3(:));
+p_sw_c_3=zeros(size(Fsw_3(:,1),1),3);
+p_pl_c_3=zeros(size(Fpl_3(:,1),1),3);
+
+
+ for  j=1:size(Fsw_3(:,1),1)%计算三轮花萼宽频数
+  for i=1:size(data_sw_3,1)
+    if (data_sw_3(i)==Fsw_3(j,1)&&data_class_3(i)==1)
+        p_sw_c_3(j,1)=p_sw_c_3(j,1)+1;
+    elseif(data_sw_3(i)==Fsw_3(j,1)&&data_class_3(i)==2)
+        p_sw_c_3(j,2)=p_sw_c_3(j,2)+1;
+    elseif(data_sw_3(i)==Fsw_3(j,1)&&data_class_3(i)==3)
+        p_sw_c_3(j,3)=p_sw_c_3(j,3)+1;
+    end
+  end
+ end
+ 
+ 
+ for  j=1:size(Fpl_3(:,1),1)%计算三轮花瓣长频数
+  for i=1:size(data_pl_3,1)
+    if (data_pl_3(i)==Fpl_3(j,1)&&data_class_3(i)==1)
+        p_pl_c_3(j,1)=p_pl_c_3(j,1)+1;
+    elseif(data_pl_3(i)==Fpl_3(j,1)&&data_class_3(i)==2)
+        p_pl_c_3(j,2)=p_pl_c_3(j,2)+1;
+    elseif(data_pl_3(i)==Fpl_3(j,1)&&data_class_3(i)==3)
+        p_pl_c_3(j,3)=p_pl_c_3(j,3)+1;
+    end
+  end
+end
+
+
+
+%统计三轮频数与频率
+Fswsum_3=[Fsw_3 p_sw_c_3+0.00000000000001];
+Fplsum_3=[Fpl_3 p_pl_c_3+0.00000000000001];
+
+
+%计算总信息熵
+Esw_3=-(sum(Fswsum_3(:,4))/sum(Fswsum_3(:,2)))*log2((sum(Fswsum_3(:,4))/sum(Fswsum_3(:,2))))-(sum(Fswsum_3(:,5))/sum(Fswsum_3(:,2)))*log2((sum(Fswsum_3(:,5))/sum(Fswsum_3(:,2))))-(sum(Fswsum_3(:,6))/sum(Fswsum_3(:,2)))*log2((sum(Fswsum_3(:,6))/sum(Fswsum_3(:,2))));
+Epl_3=-(sum(Fplsum_3(:,4))/sum(Fplsum_3(:,2)))*log2((sum(Fplsum_3(:,4))/sum(Fplsum_3(:,2))))-(sum(Fplsum_3(:,5))/sum(Fplsum_3(:,2)))*log2((sum(Fplsum_3(:,5))/sum(Fplsum_3(:,2))))-(sum(Fplsum_3(:,6))/sum(Fplsum_3(:,2)))*log2((sum(Fplsum_3(:,6))/sum(Fplsum_3(:,2))));
+
+
+Esw_fen_3=0;
+Epl_fen_3=0;
+
+
+%计算sw_3分信息熵的和
+for i=1:size(Fswsum_3,1)
+    for j=4:6
+        if(Fswsum_3(i,j)~=0)
+        Esw_fen_3=Esw_fen_3+Fswsum_3(i,2)/sum(Fswsum_3(:,2))*(-Fswsum_3(i,j)/Fswsum_3(i,2)*log2(Fswsum_3(i,j)/Fswsum_3(i,2)));
+        else
+            Esw_fen_3=Esw_fen_3+0;
+        end
+    end
+end
+
+
+%计算pl_3分信息熵的和
+for i=1:size(Fplsum_3,1)
+    for j=4:6
+        if(Fplsum_3(i,j)~=0)
+        Epl_fen_3=Epl_fen_3+Fplsum_3(i,2)/sum(Fplsum_3(:,2))*(-Fplsum_3(i,j)/Fplsum_3(i,2)*log2(Fplsum_3(i,j)/Fplsum_3(i,2)));
+        else
+            Epl_fen_3=Epl_fen_3+0;
+        end
+    end
+end
+
+
+%计算信息增益
+Gsw_3=Esw_3-Esw_fen_3;
+Gpl_3=Epl_3-Epl_fen_3;
+
